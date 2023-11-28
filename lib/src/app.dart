9fda99acc525422ab177c0f6b12351bdf39814aa
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-11-28 01:23:33 +0100

    Add boilerplate

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'screens/news_list.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'News!',
+      home: NewsList(),
+    );
+  }
+}
