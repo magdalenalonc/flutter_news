7cf444f5a40935c765055113481efd1c09e6ac6b
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-11-30 00:25:20 +0100

    Add Items Controller and single transformer

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -7,10 +7,25 @@ import '../resources/repository.dart';
 
 class StoriesBloc {
   final _repository = Repository();
+  // PublishSubject - exactly like a normal broadcast StreamController with one exception: this class is both a Stream and Sink.
   final _topIds = PublishSubject<List<int>>();
+  // BehaviorSubject - a special StreamController that captures the latest item that has been added to the controller, and emits that as the first item to any new listener.
+  final _items = BehaviorSubject<int>();
+
+  late Stream<Map<int, Future<ItemModel?>>> items;
+
 
   // getters to Streams
   Stream<List<int>> get topIds => _topIds.stream;
+
+  // getters to Sinks
+  Function(int) get fetchItem => _items.sink.add;
+
+  // constructor function which will be invoked only one time -> so it's going to be reused for every single widget that tries to subscribe to the stream.
+  // tranform - steam doesn't get modified. It returns a new stream.
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -23,15 +38,16 @@ class StoriesBloc {
 
   _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel?>>? cache, int id, index) {
-        cache?[id] = _repository.fetchItem(id);
+      (Map<int, Future<ItemModel?>> cache, int id, index) {
+        cache[id] = _repository.fetchItem(id);
         return cache;
       },
-      <int, Future<ItemModel>>{},
+      <int, Future<ItemModel?>>{},
     );
   }
 
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
