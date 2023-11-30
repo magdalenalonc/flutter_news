f5249ce542647cd9a2737d60f8648ce72b40ec4f
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-11-30 01:24:52 +0100

    Create NewsListTile and add StreamBuilder

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  const NewsListTile({super.key, required this.itemid});
+
+  final int itemid;
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel?>>> snapshot) {
+        if (!snapshot.hasData) {
+          return const Text('Stream still loading');
+        }
+
+        // TODO - return FutureBuilder
+      },
+    );
+  }
+}
