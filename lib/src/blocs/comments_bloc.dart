eb675cd8ac999be1e61c79b0b2bc45a5f6c89c73
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-03 23:58:49 +0100

    Add CommentsBloc

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,23 @@
-class CommentsBloc {}
+import 'dart:async';
+
+import 'package:rxdart/rxdart.dart';
+
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel?>>>();
+
+  // getters to Streams
+  Stream<Map<int, Future<ItemModel?>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  // getters to Sinks
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
