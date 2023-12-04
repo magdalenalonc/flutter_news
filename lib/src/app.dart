057f130595e2cbbd488259b1cec608fb5941b8d2
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-04 01:04:46 +0100

    Connect CommentsProvider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,6 +4,7 @@ import 'screens/news_list.dart';
 import 'screens/news_detail.dart';
 import '../constants.dart';
 import 'blocs/stories_provider.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -19,7 +20,11 @@ class App extends StatelessWidget {
       return MaterialPageRoute(
         builder: (context) {
           // Here is a great location to do some initialization or data fetching for NewsDetail.
+          final commentsBloc = CommentsProvider.of(context);
           final itemId = int.parse(settings.name!.replaceFirst('/', ''));
+
+          commentsBloc.fetchItemWithComments(itemId);
+          
           return NewsDetail(
             itemId: itemId,
           );
@@ -30,11 +35,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News!',
-        theme: kTheme,
-        onGenerateRoute: routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News!',
+          theme: kTheme,
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
