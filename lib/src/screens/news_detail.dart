caf8d47c643ccbcc2f51e0a219ef3e1676b199f2
Magdalena Lonc <magdalena.lonc@gmail.com>
2023-12-04 23:57:42 +0100

    Add bloc and refactor NewsDetail screen

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,12 +1,43 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import '../blocs/comments_provider.dart';
+import '../models/item_model.dart';
 
 class NewsDetail extends StatelessWidget {
   const NewsDetail({super.key, required this.itemId});
 
   final int itemId;
 
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel?>>> snapshot) {
+        if (!snapshot.hasData) {
+          return const Text('Loading');
+        }
+
+        final itemFuture = snapshot.data![itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel?> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return const Text('Loading item...');
+            }
+
+            return Text(itemSnapshot.data!.title!);
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -14,7 +45,7 @@ class NewsDetail extends StatelessWidget {
           style: Theme.of(context).textTheme.titleLarge!,
         ),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
     );
   }
 }
